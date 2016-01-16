3e9c0bd6b6f5a8f6d1882ba41f52b90b6e15c0f8
madithya2509 <madithya2509@gmail.com>
2016-01-16 12:31:47 +0530

    menu aligned

--- app/src/main/java/android/buktab/in/buktab/MainMenu2.java
+++ app/src/main/java/android/buktab/in/buktab/MainMenu2.java
@@ -57,7 +57,7 @@ public class MainMenu2 extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_main);
+        setContentView(R.layout.menu_main_2);
         fragmentManager = getSupportFragmentManager();
 
         // addFragment(new MainFragment(), true, R.id.container);
