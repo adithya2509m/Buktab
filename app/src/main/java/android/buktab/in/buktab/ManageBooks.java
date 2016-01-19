d755a23677f06d9e5b142d2240f43740757d2489
Arunkumar <ezio123arun@gmail.com>
2016-01-20 00:12:58 +0530

    finalfloat

--- app/src/main/java/android/buktab/in/buktab/ManageBooks.java
+++ app/src/main/java/android/buktab/in/buktab/ManageBooks.java
@@ -37,8 +37,10 @@ import java.util.List;
 public class ManageBooks extends Fragment {
 
     ListView lay;
+
     String url="http://52.10.251.227:3000/manageBooks";
     ArrayList<String> jasonbook,jasonauthor,jasonsem,jasonprice,jsondept,jsonid;
+    android.support.design.widget.FloatingActionButton add;
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -62,19 +64,22 @@ public class ManageBooks extends Fragment {
 
         lay = (ListView)rootView.findViewById(R.id.list);
 
+        add=(android.support.design.widget.FloatingActionButton)rootView.findViewById(R.id.fab);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }});
 
 
+            ListAdapter EventList = new customlist3(getActivity(), jasonbook, jasonsem, jasonauthor, jasonprice, jsondept, jsonid);
+            lay.setAdapter(EventList);
 
 
+            return rootView;
 
-                ListAdapter EventList= new customlist3(getActivity(),jasonbook,jasonsem,jasonauthor,jasonprice,jsondept,jsonid);
-                        lay.setAdapter(EventList);
-
-
-        return rootView;
-
-    }
-
+        }
 
 
     }
