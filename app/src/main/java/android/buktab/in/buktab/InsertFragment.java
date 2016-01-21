2f76e1431b93c515a80ca1ab776fbf7280e078ed
Arunkumar <ezio123arun@gmail.com>
2016-01-21 14:43:01 +0530

    insert

--- app/src/main/java/android/buktab/in/buktab/InsertFragment.java
+++ app/src/main/java/android/buktab/in/buktab/InsertFragment.java
@@ -2,6 +2,7 @@ package android.buktab.in.buktab;
 
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -40,6 +41,7 @@ public class InsertFragment extends Fragment {
     int count =0,pos=0;
     String sem="";
     View rootView;
+    int no=0;
 
     String[] jasonbook,jasonauthor,jasondept,jasonprice,jsonid;
 
@@ -165,7 +167,8 @@ public class InsertFragment extends Fragment {
                         }}
 
                     else{
-                        Toast.makeText(getActivity(), "No such book", Toast.LENGTH_LONG).show();
+                        no=1;
+                      //  Toast.makeText(getActivity(), "No such book", Toast.LENGTH_LONG).show();
                     }
 
 
@@ -173,7 +176,7 @@ public class InsertFragment extends Fragment {
                     return true; }
 
                 else{
-                    Toast.makeText(getActivity(), "No response from server", Toast.LENGTH_LONG).show();
+                  //  Toast.makeText(getActivity(), "No response from server", Toast.LENGTH_LONG).show();
                     return false;
                 }}
 
@@ -190,7 +193,11 @@ public class InsertFragment extends Fragment {
         protected void onPostExecute(Boolean th){
            // nDialog.dismiss();
             if(!th){
+
+                if(no==0)
                 Toast.makeText(getActivity(), "No response from server", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getActivity(), "No such book", Toast.LENGTH_LONG).show();
 
             }else{
 
@@ -200,182 +207,26 @@ public class InsertFragment extends Fragment {
                 resultlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                      //  Toast.makeText(getActivity().getApplicationContext(),"Heloo",Toast.LENGTH_SHORT).show();
-                        final Dialog dialog = new Dialog(getActivity(),R.style.NewDialog);
+                        //  Toast.makeText(getActivity().getApplicationContext(),"Heloo",Toast.LENGTH_SHORT).show();
 
-                        dialog.setContentView(R.layout.insert_dialog);
-                       // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
-                        dialog.setTitle("Insert");
 
-                        dialog.show();
-                        final Spinner spinner = (Spinner) dialog.findViewById(R.id.sem_spinner);
+                        Intent i = new Intent(getActivity(), Insertintent.class);
+                        i.putExtra("bookname",jasonbook[position]);
+                        i.putExtra("author",jasonauthor[position]);
+                        i.putExtra("dept",jasondept[position]);
+                        i.putExtra("id",jsonid[pos]);
 
-                        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                                R.array.sem_array, android.R.layout.simple_spinner_item);
-
-                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-                        spinner.setAdapter(adapter);
+                        startActivity(i);
 
 
 
-                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                                sem = parent.getItemAtPosition(pos).toString();
-                            }
-                            public void onNothingSelected(AdapterView<?> parent) {
-                            }
-                        });
-
-                        TextView bname= (TextView) dialog.findViewById(R.id.bname);
-                        TextView aname= (TextView) dialog.findViewById(R.id.aname);
-                        TextView sname= (TextView) dialog.findViewById(R.id.sname);
-                         price=(EditText)dialog.findViewById(R.id.price);
-                        Button dismiss=(Button)dialog.findViewById(R.id.dismiss);
-                        Button insert =(Button)dialog.findViewById(R.id.insert);
-                        bname.setText(jasonbook[position]);
-                        aname.setText(jasonauthor[position]);
-                        sname.setText(jasondept[position]);
-                        dismiss.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                        insert.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                ConnectionDetector cd=new ConnectionDetector(getActivity().getApplicationContext());
-                                if (cd.isConnectingToInternet()) {
-                                    pos=position;
-                                    if(price.getText().toString().length()==0) {
-                                        price.setError("Set Price");
-                                    }
-                                    if(sem.length()==0){
-
-                                        Toast.makeText(getActivity(), "Select Sem", Toast.LENGTH_LONG).show();
-                                    }else{
-                                        startAnim();
-                                        new Insert().execute();
-                                    }
-                                } else {
-                                    Toast.makeText(getActivity(), "No Internet Connection", Toast.LENGTH_LONG).show();
-                                }
-                                dialog.dismiss();
-                            }
-                        });
-
-
-                    }
-                });
 
             }
 
 
-        }
-    }
+        });
+    }}}
 
-    private class Insert extends AsyncTask<String,String,Boolean>
-    {
-        private ProgressDialog nDialog;
-        EditText temp;
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-        /*   nDialog = new ProgressDialog(getActivity());
-            nDialog.setTitle("Sending data to server");
-            nDialog.setMessage("Please Wait..");
-            nDialog.setIndeterminate(false);
-            nDialog.setCancelable(true);
-            nDialog.show();*/
-            //Toast.makeText(getContext(),"fetching results",Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        protected Boolean doInBackground(String... args){
-            GPSTracker gps;
-            gps = new GPSTracker(getActivity().getApplicationContext());
-            double latitude = 10,longitude=10;
-            if(gps.canGetLocation()) {
-
-                latitude = gps.getLatitude();
-                longitude = gps.getLongitude();
-            }else{
-
-                gps.showSettingsAlert();
-            }
-
-                JSONObject jsonobject;
-            final JSONParser jParser2 = new JSONParser();
-            List<NameValuePair> params2 = new ArrayList<NameValuePair>();
-            params2.add(new BasicNameValuePair("price", price.getText().toString()));
-            params2.add(new BasicNameValuePair("sem",sem));
-
-            params2.add(new BasicNameValuePair("bookid", jsonid[pos]));
-            params2.add(new BasicNameValuePair("lat", String.valueOf(latitude)));
-            params2.add(new BasicNameValuePair("long", String.valueOf(longitude)));
-
-            params2.add(new BasicNameValuePair("token", Login.token));
-
-            jsonobject = jParser2.makeHttpRequest(posturl, "POST", params2);
-
-            try{
-                if(jsonobject!=null){
-
-                    String result=jsonobject.getString("success");
-
-                    if(result.equals("true"))
-                    {
-                        Splash.jasonbook.add(jasonbook[pos]);
-                        Splash.jasonauthor.add(jasonauthor[pos]);
-                        Splash.jasonsem.add(sem);
-                        Splash.jasonprice.add(price.getText().toString());
-                        Splash.jsondept.add(jasondept[pos]);
-                        Splash.jsonid.add(jsonobject.getJSONObject("post").getString("_id"));
-                        return true;
-                        }
-
-                    else{
-                     return false;
-                    }
-
-
-
-                     }
-
-                else{
-                    //Toast.makeText(getContext(), "No response from server", Toast.LENGTH_LONG).show();
-                    return false;
-                }}
-
-
-
-            catch (JSONException e){
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return false;
-
-        }
-        @Override
-        protected void onPostExecute(Boolean th){
-           // nDialog.dismiss();
-            if(!th){
-                stopAnim();
-                Toast.makeText(getActivity(), "Insert Failed", Toast.LENGTH_LONG).show();
-
-            }else{
-                stopAnim();
-                Toast.makeText(getActivity(), "Insert Successful", Toast.LENGTH_LONG).show();
-                Splash.bookcount++;
-                MainMenu.bcnt.setText("" + Splash.bookcount);
-            }
-
-
-        }
-    }
 
 
 
